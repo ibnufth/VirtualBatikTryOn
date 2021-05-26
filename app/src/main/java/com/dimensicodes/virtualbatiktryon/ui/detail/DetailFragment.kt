970161cfab3bc113b667deb09b53e12c4074665d
ibnufth <ibnufth@users.noreply.github.com>
2021-05-26 11:47:21 +0700

    commit export image to gallery

--- app/src/main/java/com/dimensicodes/virtualbatiktryon/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/dimensicodes/virtualbatiktryon/ui/detail/DetailFragment.kt
@@ -1,16 +1,25 @@
 package com.dimensicodes.virtualbatiktryon.ui.detail
 
+import android.Manifest
+import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import com.dimensicodes.virtualbatiktryon.R
 import com.dimensicodes.virtualbatiktryon.databinding.FragmentDetailBinding
+import com.dimensicodes.virtualbatiktryon.util.ExportImageHelper
 
 class DetailFragment : Fragment() {
 
-    private lateinit var detailFragmentBinding : FragmentDetailBinding
+    private lateinit var detailFragmentBinding: FragmentDetailBinding
+
+
+    companion object {
+        private const val TAG = "DetailFragment"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +31,24 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        ActivityCompat.requestPermissions(
+            context as Activity,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            1
+        )
+        ActivityCompat.requestPermissions(
+            context as Activity,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            1
+        )
+        detailFragmentBinding.btnSave.setOnClickListener {
+            detailFragmentBinding.btnSave.visibility = View.GONE
+            val export = ExportImageHelper(context as Activity)
+            export.saveToStorage(view.rootView.findViewById(R.id.mainView))
+            detailFragmentBinding.btnSave.visibility = View.VISIBLE
+        }
 
     }
+
+
 }
